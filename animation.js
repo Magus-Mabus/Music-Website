1d471dc32629dce36833fa9bb1f6ed746013b885
Sebastian Park <SebPark03@gmail.com>
2020-05-16 21:49:55 -0400

    Make font sizes scale.
    May change back later.

--- animation.js
+++ animation.js
@@ -6,18 +6,20 @@ function scrollFunction() {
     var particleCanvas = document.getElementById("particles-js").style;
     var mainTitle = document.getElementById("scroll-up-smaller").style;
     if (document.body.scrollTop / document.body.clientHeight > 50 / 790
-        || document.documentElement.scrollTop / document.body.clientHeight > 50/790) {
+        || document.documentElement.scrollTop / document.body.clientHeight > 50 / 790) {
         mainTitle.padding = "0px 0px";
         mainTitle.backgroundColor = "rgba(248, 169, 244, 0.6)";
         mainTitle.top = "-1%";
-        mainTitle.fontSize = "60px";
+        mainTitle.fontSize = "4vw";
+        // mainTitle.fontSize = "60px";
         mainTitle.fontWeight = "100";
         // particleCanvas.opacity = "0";
     } else {
         mainTitle.padding = "20px 20px";
         mainTitle.backgroundColor = "rgba(255, 105, 180, 0)";
         mainTitle.top = "40%";
-        mainTitle.fontSize = "100px";
+        // mainTitle.fontSize = "100px";
+        mainTitle.fontSize = "7vw";
         particleCanvas.opacity = "1";
     }
 }
