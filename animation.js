1e71821008c4da473f4f2482ac99fa6471f2461a
Sebastian Park <SebPark03@gmail.com>
2021-04-18 22:27:26 -0400

    Change title animation.

--- animation.js
+++ animation.js
@@ -4,6 +4,7 @@ var backgroundVideo = document.getElementById("bg-video");
 var backgroundBlurrer = document.getElementById("background-blurrer");
 var pageFooter = document.getElementById("page-footer");
 var preloadVideo = document.getElementById("image-preload");
+var header = document.getElementById("main-header");
 var titleSize = "100px";
 var width;
 var headerSize = "60px";
@@ -17,43 +18,57 @@ function scrollFunction() {
     console.log(document.body.scrollTop);
     if (document.body.scrollTop / document.body.clientHeight > 50 / 790
         || document.documentElement.scrollTop / document.body.clientHeight > 50 / 790) {
-        mainTitle.padding = "0px 0px";
-        mainTitle.backgroundColor = "var(--header-bg-color)";
-        // mainTitle.backdropFilter = "blur(20px)";
-        // mainTitle.webkitBackdropFilter = "blur(20px)";
-        mainTitle.top = "-1%";
-        // mainTitle.fontSize = "4vw";
+        //DOWN
+            //ORIGINAL TITLE ANIMATION
+        // mainTitle.padding = "0px 0px";
+        // mainTitle.backgroundColor = "var(--header-bg-color)";
+        // // mainTitle.backdropFilter = "blur(20px)";
+        // // mainTitle.webkitBackdropFilter = "blur(20px)";
+        // mainTitle.top = "-1%";
+        // // mainTitle.fontSize = "4vw";
+        // // mainTitle.borderBottom = "1px solid rgb(200,200,200)";
+        // mainTitle.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.199)";
+        // // particleCanvas.opacity = "0";
+        // mainTitle.color = "black";
+        // mainTitle.fontWeight = "var(--big-font-title-card-weight)";
+        // mainTitle.backgroundClip = "border-box";
+        // mainTitle.webkitBackgroundClip = "border-box";
         mainTitle.fontSize = headerSize;
-        // mainTitle.borderBottom = "1px solid rgb(200,200,200)";
-        mainTitle.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.199)";
-        // particleCanvas.opacity = "0";
-        mainTitle.color = "black";
-        mainTitle.fontWeight = "var(--big-font-title-card-weight)";
+        mainTitle.opacity = "0";
+        header.style.opacity = "1";
         backgroundVideo.style.opacity = "0";
         backgroundBlurrer.style.opacity = "0";
         pageFooter.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
+        header.style.transform = "scale(1,1) translate(0%,50%)";
+        particleCanvas.opacity = "1";
         preloadVideo.style.opacity = "0";
     } else {
-        mainTitle.padding = "20px 20px";
+        //TOP
+        //ORIGINAL TITLE ANIMATION
+        /*mainTitle.padding = "20px 20px";
         mainTitle.backgroundColor = "transparent";
         // mainTitle.webkitBackdropFilter = "none";
         mainTitle.top = "40%";
         mainTitle.backdropFilter = "none";
-        mainTitle.fontSize = titleSize;
         // mainTitle.fontSize = "7vw";
-        particleCanvas.opacity = "1";
         mainTitle.borderBottom = "none";
         mainTitle.boxShadow = "none";
         mainTitle.color = "white";
         mainTitle.fontWeight = "var(--big-font-title-weight)";
+        mainTitle.backgroundClip = "text";
+        mainTitle.webkitBackgroundClip = "text";*/
+        particleCanvas.opacity = "0";
         backgroundVideo.style.opacity = "1";
         backgroundBlurrer.style.opacity = "1";
         pageFooter.style.backgroundColor = "transparent";
         preloadVideo.style.opacity = "1";
+        mainTitle.fontSize = titleSize;
+        mainTitle.opacity = "1";
+        header.style.transform = "scale(1,0) translate(0%,50%)";
     }
 }
 
-function resizeFunction(){
+function resizeFunction() {
     width = document.body.clientWidth;
     if (width > 600) {
         headerSize = "60px";
